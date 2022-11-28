63d71098c4d455bc6b1a3c536a31a3fa084f5ebc
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-28 16:01:37 -0700

    GET /restaurants tested and built

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -20,11 +20,52 @@ const registerAndLogin = async (userProps = {}) => {
   return [agent, user];
 };
 
-describe.skip('restaurant routes', () => {
+describe('restaurant routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   afterAll(() => {
     pool.end();
+  });
+
+  test('GET api/v1/restaurants returns a list of restaurants', async () => {
+    const resp = await request(app).get('/api/v1/restaurants');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "cost": 1,
+          "cuisine": "1",
+          "id": "1",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+          "name": "1",
+          "website": "http://www.PipsOriginal.com",
+        },
+        Object {
+          "cost": 3,
+          "cuisine": "2",
+          "id": "2",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
+          "name": "2",
+          "website": "http://www.muccaosteria.com",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "3",
+          "id": "3",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
+          "name": "3",
+          "website": "http://www.mediterraneanexplorationcompany.com/",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "4",
+          "id": "4",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
+          "name": "4",
+          "website": "https://saltandstraw.com/pages/nw-23",
+        },
+      ]
+    `);
   });
 });
